b713c4283a67efd52b181ae968534101caff7ebe
kyawkhaing <kyawkhaing.kk003@gmail.com>
2024-01-09 11:39:11 +0630

    header

--- src/main/java/com/example/booksshop/BooksShopApplication.java
+++ src/main/java/com/example/booksshop/BooksShopApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
@@ -26,6 +27,7 @@ public class BooksShopApplication {
 
     @Bean
     @Transactional
+    @Profile("data")
     public ApplicationRunner runner() {
         return r -> {
             Author author1 = new Author("Charles Dickens", "cgarles@gmail.com");
--- src/main/java/com/example/booksshop/controller/AppController.java
+++
@@ -1,7 +0,0 @@
-package com.example.booksshop.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AppController {
-}
