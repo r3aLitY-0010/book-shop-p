ba2315d2c582fd8076daf390f962ffb77995feac
kyawkhaing <kyawkhaing.kk003@gmail.com>
2024-01-09 10:07:23 +0630

    Filled with Data

--- src/main/java/com/example/booksshop/entity/Book.java
+++ src/main/java/com/example/booksshop/entity/Book.java
@@ -36,14 +36,16 @@ public class Book {
     private String description;
     private double price;
     private int stock;
+    private String imgUrl;
 
-    public Book(int id, String isbn, String title, String description, double price, int stock) {
+    public Book(int id, String isbn, String title, String description, double price, int stock, String imgUrl) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
         this.description = description;
         this.price = price;
         this.stock = stock;
+        this.imgUrl = imgUrl;
     }
 
     public void addGenres(Genre genre) {
