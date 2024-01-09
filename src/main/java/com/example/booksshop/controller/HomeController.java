ba68147c7af81a1bfe76690946b8a8d8f07570d3
kyawkhaing <kyawkhaing.kk003@gmail.com>
2024-01-09 14:54:16 +0630

    landing page

--- src/main/java/com/example/booksshop/controller/HomeController.java
+++ src/main/java/com/example/booksshop/controller/HomeController.java
@@ -1,12 +1,22 @@
 package com.example.booksshop.controller;
 
+import com.example.booksshop.dao.BookDao;
+import com.example.booksshop.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final BookService bookService;
+
     @GetMapping({"/", "/home"})
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("books", bookService.listAllBooks());
         return "home";
     }
+
+
 }
---
+++ src/main/java/com/example/booksshop/service/BookService.java
@@ -0,0 +1,19 @@
+package com.example.booksshop.service;
+
+import com.example.booksshop.dao.BookDao;
+import com.example.booksshop.entity.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookDao bookDao;
+
+    public List<Book> listAllBooks() {
+        return bookDao.findAll();
+    }
+
+}
